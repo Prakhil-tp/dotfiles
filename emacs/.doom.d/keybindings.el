fc1f0ab2987682219c386cd4e9f9a119499b8ce1
prakhil-tp <prakhil.tp@gmail.com>
2021-07-04 01:07:44 +0530

    feat: Add emacs configuration

---
+++ emacs/.doom.d/keybindings.el
@@ -0,0 +1,14 @@
+;;; keybindings.el -*- lexical-binding: t; -*-
+
+;; window switching
+(map!
+ "C-l" #'evil-window-right
+ "C-h" #'evil-window-left
+ "C-k" #'evil-window-up
+ "C-j" #'evil-window-down)
+
+;; toggle display number
+(map! :leader 
+      :desc "set number" "n u" #'menu-bar--display-line-numbers-mode-absolute 
+      :desc "set relative number" "r n u" #'menu-bar--display-line-numbers-mode-relative 
+       )
