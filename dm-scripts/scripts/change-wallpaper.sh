bab9c43758e22f2b8b35ad1adbd92a38dc31db31
prakhil-tp <20636485+Prakhil-tp@users.noreply.github.com>
2022-11-09 20:54:53 +0530

    feat: make it efficient

--- dm-scripts/scripts/change-wallpaper.sh
+++ dm-scripts/scripts/change-wallpaper.sh
@@ -1,11 +1,16 @@
-#! /bin/bash
-#using the path WALLPAPERS will select a new random wallpaper
-WALLPAPERS="/home/prakhil/Pictures/backgrounds"
-ALIST=( `ls -w1 $WALLPAPERS` )
-RANGE=${#ALIST[@]}
-let "number = $RANDOM"
-let LASTNUM="`cat $WALLPAPERS/.last` + $number"
-let "number = $LASTNUM % $RANGE"
-echo $number > $WALLPAPERS/.last
-nitrogen --set-scaled --save $WALLPAPERS/${ALIST[$number]}
+#!/bin/sh
+
+export DISPLAY=:0 #initializing the display for CRON job support
+wallpaper_path="/home/prakhil/Pictures/wallpapers"
+list=( `ls -w1 $wallpaper_path | grep "[a-zA-Z0-9-]*\.\(jpg\|png\)"` )
+list_length=${#list[@]}
+let "number = $RANDOM % $list_length"
+let "last_num=`cat $wallpaper_path/.last`"
+while [ $last_num == $number ]; do
+  let "number = $RANDOM % $list_length"
+done
+echo $number > $wallpaper_path/.last
+#nitrogen --set-scaled --save $WALLPAPERS/${ALIST[$number]}
+feh --bg-fill $wallpaper_path/${list[$number]}
+echo "LAST_WALLPAPER=\"$wallpaper_path/${list[$number]}\"" > /tmp/wallpaper.log
 exit 
