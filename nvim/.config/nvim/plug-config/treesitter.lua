d44ac648238bf73de13ed2fd7b5cb9a39c287590
prakhil-tp <20636485+Prakhil-tp@users.noreply.github.com>
2022-11-09 21:07:48 +0530

    feat: add parser config

--- nvim/.config/nvim/plug-config/treesitter.lua
+++ nvim/.config/nvim/plug-config/treesitter.lua
@@ -23,3 +23,6 @@ configs.setup({
 	rainbow = { enable = true, extended_mode = true, max_file_lines = nil },
   playground = { enable = true }
 })
+
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
