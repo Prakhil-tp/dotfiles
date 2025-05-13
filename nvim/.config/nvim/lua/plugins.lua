d99a2525d88692f86d1ab6412875648bcdec8b4a
prakhil-tp <20636485+Prakhil-tp@users.noreply.github.com>
2025-05-13 21:37:58 +0400

    Add more plugins

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -52,7 +52,7 @@ return {
       playground = {
         enable = true,
         disable = {},
-        updatetime = 25,        -- Debounced time for highlighting nodes in the playground from source code
+        updatetime = 25,         -- Debounced time for highlighting nodes in the playground from source code
         persist_queries = false, -- Whether the query persists across vim sessions
         keybindings = {
           toggle_query_editor = 'o',
@@ -166,5 +166,34 @@ return {
   {
     'numToStr/Comment.nvim',
     opts = {}
+  },
+  {
+    "karb94/neoscroll.nvim",
+    opts = {
+      mappings = { -- Keys to be mapped to their corresponding default scrolling animation
+        '<C-u>', '<C-d>',
+        '<C-b>', '<C-f>',
+        '<C-y>', '<C-e>',
+        'zt', 'zz', 'zb',
+      },
+      hide_cursor = true,          -- Hide cursor while scrolling
+      stop_eof = true,             -- Stop at <EOF> when scrolling downwards
+      respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+      cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+      duration_multiplier = 1.0,   -- Global duration multiplier
+      easing = 'linear',           -- Default easing function
+      pre_hook = nil,              -- Function to run before the scrolling animation starts
+      post_hook = nil,             -- Function to run after the scrolling animation ends
+      performance_mode = false,    -- Disable "Performance Mode" on all buffers.
+      ignored_events = {           -- Events ignored while scrolling
+        'WinScrolled', 'CursorMoved'
+      },
+    },
+  },
+  {
+    "norcalli/nvim-colorizer.lua",
+    config = function()
+      require("colorizer").setup()
+    end,
   }
 }
